1f9eeb97c676fa2a8d1916a78ac3d3505d2ceeb5
adrifuente <55239087+adrifuente@users.noreply.github.com>
2020-10-15 18:52:59 +0200

    Creación de petición ajax y modificación en la estructura de los js

--- webroot/js/home.js
+++ webroot/js/home.js
@@ -12,31 +12,21 @@ let Home = {
     init: function () {
         // LLamada a eventos
         this.events();
+        // LLamada a cargar lista
+        this.loadLists();
     },
     // Eventos
-    events: function () {
-        window.onload = function () {
-            Cargar();    
-        }
-        // Evento desplegar input buscar
-        $(".fa-search").on("click", function (event) {
-            $("#input-custom").removeClass("d-none");
-            $(".navbar-icons").addClass("d-none");
-        });
-        // Evento desplegar input guardar
-        $(".fa-plus").on("click", function (event) {
-            $("#input-custom").removeClass("d-none");
-            $(".navbar-icons").addClass("d-none");
-            $(".input-custom").attr("placeholder","Guardar una URL https//...");
-            $(".btn-input-custom1").text("Guardar");
-        });
-        $(".btn-input-custom1").on("click", function (event) {
-            $("#input-custom").addClass("d-none");
-            $(".navbar-icons").removeClass("d-none");
-        });
-        $(".btn-input-custom2").on("click", function (event) {
-            $("#input-custom").addClass("d-none");
-            $(".navbar-icons").removeClass("d-none");
+    events: function () {},
+    loadLists: function () {
+        $.ajax({
+            type: "GET",
+            url: "/home/results",
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("X-CSRF-Token", $('[name="_csrfToken"]').val());
+            },
+            success: function (data) {
+                // $('.results').html(data);
+            },
         });
     },
 };
