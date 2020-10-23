2ffeccf86318892bb90f0de71a18cc1243eddf7c
adrifuente <55239087+adrifuente@users.noreply.github.com>
2020-10-23 14:14:34 +0200

    Mostrar favoritos y borrar items

--- webroot/js/results.js
+++ webroot/js/results.js
@@ -12,25 +12,44 @@ let Results = {
     init: function () {
         // LLamada a eventos
         this.events();
+        // LLamada a cargar lista
+        this.loadLists();
+        // LLamada a favoritos
+        this.favourites();
+        // LLamada a inicializacion atributos
+        this.attributes();
+    },
+    attributes: function () {
+        this.toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+        });
     },
     // Eventos
     events: function () {
-        $(".results .fa-star").on("click", function () {
-            if($(this).css("color") == "gold"){
-                $(this).css("color","#213e03");
-            }else{
+        $(document).on("click", ".favItem", function () {
+            if($(this).css("color","#213e03")){
                 $(this).css("color","gold");
+            }else{
+                $(this).css("color","#213e03");
             }
         });
-        $(".fa-trash-alt").on("click", function () {
-            alert("hola");
-            //Results.delete();
+        $(document).on("click", ".deleteItem", function () {
+            let url = $(this).data('url');
+            let message = $(this).data('message');
+            let ok = $(this).data('ok');
+            let cancel = $(this).data('cancel');
+            let data = {url: url, message: message, ok: ok, cancel: cancel};
+            Results.delete(data);
         });
     },
-    delete: function () {
+    loadLists: function () {
         $.ajax({
             type: "GET",
-            url: "/home/delete",
+            url: "/home/results",
             beforeSend: function (xhr) {
                 xhr.setRequestHeader(
                     "X-CSRF-Token",
@@ -39,6 +58,55 @@ let Results = {
             },
             success: function (data) {
                 $(".results").html(data);
+            },
+        });
+    },
+    delete: function (data) {
+        Swal.fire({
+            title: data.message,
+            icon: "question",
+            showCancelButton: true,
+            cancelButtonColor: "#d33",
+            confirmButtonText: data.ok,
+            cancelButtonText: data.cancel,
+        }).then(result => {
+            if (result.value) {
+                $.ajax({
+                    type: "GET",
+                    url: data.url,
+                    beforeSend: function (xhr) {
+                        xhr.setRequestHeader("X-CSRF-Token", $('[name="_csrfToken"]').val());
+                    },
+                    success: function (data) {
+                        if (data.deleted) {
+                            Results.loadLists();
+                            Results.toast.fire({
+                                icon: "success",
+                                title: data.message
+                            });
+                        } else {
+                            Results.toast.fire({
+                                icon: "error",
+                                title: data.message
+                            });
+                        }
+                    },
+                });
+            }
+        });
+    },
+    favourites: function () {
+        $.ajax({
+            type: "GET",
+            url: "/favourites/results",
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader(
+                    "X-CSRF-Token",
+                    $('[name="_csrfToken"]').val()
+                );
+            },
+            success: function (data) {
+                $(".resultsFavs").html(data);
             },
         });
     },
