ef4aec463505baecb5cff03ec91881b40be2b40a
adrifuente <55239087+adrifuente@users.noreply.github.com>
2020-10-27 17:47:15 +0100

    Creación del controlador y vista favourites

--- src/Controller/FavouritesController.php
+++ src/Controller/FavouritesController.php
@@ -1,16 +1,9 @@
 <?php
 namespace App\Controller;
 
-use App\Controller\AppController;
-
-/**
- * Favourites Controller
- *
- *
- * @method \App\Model\Entity\Favourite[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
- */
 class FavouritesController extends AppController
 {
+
     public function initialize()
     {
         parent::initialize();
@@ -18,12 +11,6 @@ class FavouritesController extends AppController
         $this->loadModel('Lists');
         $this->loadModel('Items');
     }
-
-    /**
-     * Index method
-     *
-     * @return \Cake\Http\Response|null
-     */
 
     public function index()
     {
@@ -44,40 +31,28 @@ class FavouritesController extends AppController
     // Función para buscar las listas
     public function results()
     {
-        $items = [];
+        $lists = [];
         // Recogemos identidad de la session
         $user_id = $this->request->getSession()->read('Auth.User.id');
         if ($user_id) {
-            // Buscamos datos en db para las listas
-            $items = $this->Items->find('all', [
+            // Buscamos datos en db para los items
+            $lists = $this->Lists->find('all', [
                 'conditions' => [
-                    'Items.id_fav' => 1,
-                    //'Lists.id_user' => $user_id,
+                    'Lists.id_user' => $user_id,
                 ],
             ])->toArray();
+            foreach ($lists as $list) {
+                $list['items'] = $this->Items->find('all', [
+                    'conditions' => [
+                        'Items.is_fav' => 1,
+                        'Items.id_list' => $list->id
+                    ],
+                ])->toArray();
+            }
         }
         $this->viewBuilder()->setLayout('ajax');
         $this->set([
-            'items' => $items,
+            'lists' => $lists,
         ]);
-    }
-    
-    // Función para borrar items
-    public function delete($id)
-    {
-        $item = $this->Items->get($id);
-        if ($this->Items->delete($item)) {
-            $deleted = true;
-            $message = __('Item eliminado correctamente');
-        } else {
-            $deleted = false;
-            $message = __('Se produjo un error al elminar el item');
-        }
-        $this->set([
-            'deleted' => $deleted,
-            'message' => $message,
-            '_serialize' => ['deleted', 'message']
-        ]);
-        $this->RequestHandler->renderAs($this, 'json');
     }
 }
