07386d359de087ae760605f77c2fb41e6f1e9f42
Joel <enigmo@enigmo.es>
2020-11-21 18:27:16 +0100

    Corregir función imageToBase64
    Modificar lógica para retornar null en caso de no poder cargar la imagen

--- config/utils.php
+++ config/utils.php
@@ -185,8 +185,12 @@ class Functions {
     public static function imageToBase64($urlImage)
     {
         $file_info = new \finfo(FILEINFO_MIME_TYPE);
-        $mime_type = $file_info->buffer(file_get_contents($urlImage));
-        $image = 'data:' . $mime_type . ';base64,' . base64_encode(file_get_contents($urlImage));
+        $content = @file_get_contents($urlImage);
+        if ($content) {
+            $image = 'data:' . $file_info->buffer($content) . ';base64,' . base64_encode($content);
+        } else {
+            $image = null;
+        }
         return $image;
     }
 }
